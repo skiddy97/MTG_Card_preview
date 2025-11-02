db3b9c93f1f15b66bfa99ecc407d91659f493e4b
skiddy97 <151096808+skiddy97@users.noreply.github.com>
2025-11-02 16:31:59 +0100

    fetched data now displayed
    added visuals
    changed {*} to symbols

--- script.js
+++ script.js
@@ -1,18 +1,93 @@
+var section = $(".main__section");
+var cardImage = $("#card__image");
+var cardHeader = $(".card__header");
+var cardTitle = $("#card__title");
+var cardEffect = $("#card__effect");
+var cardFlavor = $("#card__flavor");
+var cardSet = $("#card__set");
+var cardType = $("#card__type");
+var cardMana = $("#card__mana");
+var cardRarity = $("#card__rarity");
+var cardAngDef = $("#card__angdef");
+var cardText = $("#card__text");
+let cardSymbols;
+
 async function fetchCard() {
-	setTimeout(() => {
-		fetch("https://api.scryfall.com/cards/random")
-			.then((response) => response.json())
-			.then((data) => displayCard(data));
-	}, "500");
+	fetch("https://api.scryfall.com/cards/random")
+		.then((response) => response.json())
+		.then((data) => displayCard(data));
 }
 function displayCard(data) {
-	createLayout(data);
+	createCardImage(data);
+	createCardTitle(data);
+	createCardEffect(data);
+	createCardFlavorText(data);
+	createCardMana(data);
+	createCardType(data);
+	createCardAngDef(data);
+	createCardSet(data);
+	createCardRarity(data);
 }
 
-function createLayout(data) {
-	const card = $("<img>").addClass("black");
-	card.attr("src", data.image_uris.normal);
-	$("body").append(card);
+function createCardImage(data) {
+	cardImage.attr("src", data.image_uris.normal);
 }
 
-fetchCard();
+function createCardTitle(data) {
+	cardTitle.text(data.name);
+}
+
+function createCardMana(data) {
+	cardMana.html(replaceBrackets(data.mana_cost));
+}
+
+function createCardType(data) {
+	cardType.text(data.type_line);
+}
+
+function createCardAngDef(data) {
+	if (data.power !== undefined && data.power !== undefined) {
+		cardAngDef.text(`( ${data.power} / ${data.toughness} )`);
+	} else {
+		cardAngDef.text("");
+	}
+}
+
+function createCardSet(data) {
+	cardSet.attr("src", data.set_uri);
+}
+
+function createCardEffect(data) {
+	cardEffect.html(replaceBrackets(data.oracle_text));
+}
+
+function createCardFlavorText(data) {
+	cardFlavor.text(data.flavor_text);
+}
+
+var reload = document.getElementById("fetchCard");
+console.log(reload);
+reload.addEventListener("click", fetchCard);
+async function fetchCardSymbol() {
+	fetch("https://api.scryfall.com/symbology ")
+		.then((response) => response.json())
+		.then((object) => object.data)
+		.then((data) => {
+			return (cardSymbols = data);
+		});
+}
+function replaceSymbol(data) {}
+
+fetchCardSymbol();
+
+function replaceBrackets(string) {
+	cardSymbols.forEach((element) => {
+		string = string.replaceAll(
+			element.symbol,
+			'<span class="symbol__container"><img class="card__symbols"src="' +
+				element.svg_uri +
+				'"></span>'
+		);
+	});
+	return string;
+}
